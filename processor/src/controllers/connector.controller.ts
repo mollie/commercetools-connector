672c12f11097ef0a938b3681d030268257ba91fc
Win <t.dang@shopmacher.de>
2024-09-09 16:38:31 +0700

    MOL-401: add mollie status checking endpoint

--- processor/src/controllers/connector.controller.ts
+++ processor/src/controllers/connector.controller.ts
@@ -1,18 +1,35 @@
-import { apiSuccess } from '../api/success.api';
 import { logger } from '../utils/logger.utils';
 import { Request, Response } from 'express';
 import CustomError from '../errors/custom.error';
 import { apiError } from '../api/error.api';
 import { formatErrorResponse } from '../errors/mollie.error';
 import { createExtensionAndCustomFields, removeExtension } from '../service/connector.service';
+import { getProfile } from '../mollie/profile.mollie';
 
 export const healthCheck = async (request: Request, response: Response) => {
   try {
     logger.debug('SCTM - healthCheck - The connector is running healthily.');
-    return apiSuccess(200, response, []);
+    return response.status(200).json('The connector is running healthily.');
   } catch (error) {
     logger.error('SCTM - healthCheck - Unexpected error occurred when processing request', error);
     return apiError(response, formatErrorResponse(error).errors);
+  }
+};
+
+export const mollieStatus = async (request: Request, response: Response) => {
+  try {
+    logger.debug('SCTM - mollieStatus - Checking Mollie API status.');
+    const profile = await getProfile();
+    logger.debug('SCTM - mollieStatus - Mollie API status is functioning.');
+    return response.status(200).json({
+      mode: profile.mode,
+      name: profile.name,
+      website: profile.website,
+      status: profile.status,
+    });
+  } catch (error) {
+    logger.error('SCTM - healthCheck - Unexpected error occurred when processing request', error);
+    return response.status(400).json(error).send();
   }
 };
 
@@ -29,7 +46,9 @@ export const install = async (request: Request, response: Response) => {
     logger.debug(
       'SCTM - install - The connector was installed successfully with required extensions and custom fields.',
     );
-    return apiSuccess(200, response, []);
+    return response
+      .status(200)
+      .json('The connector was installed successfully with required extensions and custom fields.');
   } catch (error) {
     logger.error('SCTM - install - Unexpected error occurred when processing request', error);
     return apiError(response, formatErrorResponse(error).errors);
@@ -40,7 +59,7 @@ export const uninstall = async (request: Request, response: Response) => {
   try {
     await removeExtension();
     logger.debug('SCTM - uninstall - The connector was uninstalled successfully.');
-    return apiSuccess(200, response, []);
+    return response.status(200).json('The connector was uninstalled successfully.');
   } catch (error) {
     logger.error('SCTM - uninstallation - Unexpected error occurred when processing request', error);
     return apiError(response, formatErrorResponse(error).errors);
