159fe402e9da6b00b0e9486e0ffa6b7826cc35d8
Win <t.dang@shopmacher.de>
2024-09-10 10:49:00 +0700

    MOL-357/PICT-241: update extension url

--- processor/src/controllers/connector.controller.ts
+++ processor/src/controllers/connector.controller.ts
@@ -16,7 +16,7 @@ export const healthCheck = async (request: Request, response: Response) => {
 };
 
 export const install = async (request: Request, response: Response) => {
-  const { extensionUrl } = request.body;
+  const extensionUrl = `${request.protocol}://${request.hostname}/processor`;
 
   if (!extensionUrl) {
     logger.debug('SCTM - install - Missing body parameters {extensionUrl}.');
