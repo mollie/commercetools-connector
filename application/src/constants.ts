aa16e97b2d3980116ab1ac71958ce2fbaaa4c7ad
Win <t.dang@shopmacher.de>
2025-03-31 11:17:17 +0700

    [UPDATE] release version

--- application/src/constants.ts
+++ application/src/constants.ts
@@ -12,8 +12,8 @@ export const EXTENSION_KEY = 'sctm-payment-create-update-extension';
 export const EXTENSION_URL_PATH = '/processor';
 export const APPLICATION_URL_PATH = '/application/methods';
 export const USER_AGENT = {
-  name: 'ShopmacherMollieCommercetoolsConnector/1.3.1',
-  version: '1.3.1',
-  libraryName: 'ShopmacherMollieCommercetoolsConnector/1.3.1',
+  name: 'ShopmacherMollieCommercetoolsConnector/1.3.2',
+  version: '1.3.2',
+  libraryName: 'ShopmacherMollieCommercetoolsConnector/1.3.2',
   contactEmail: 'info@mollie.com',
 };
